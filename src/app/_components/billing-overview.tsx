54fa53423c4d2d81181fb3e27c809132565020a8
Nico Vogel <nico-vogel97@outlook.de>
2024-04-25 11:37:59 +0200

    build view for billing

--- src/app/_components/billing-overview.tsx
+++ src/app/_components/billing-overview.tsx
@@ -1,7 +1,26 @@
 "use client";
 
-import { graphql } from "gql.tada";
+import { DialogTitle } from "@radix-ui/react-dialog";
 import { useQuery } from "@urql/next";
+import { graphql } from "gql.tada";
+import { useEffect, useState } from "react";
+import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTrigger,
+} from "~/components/ui/dialog";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
 
 const CreditBalanceQuery = graphql(`
   query GetCreditBalance($customerId: String!) {
@@ -17,11 +36,127 @@ const CreditBalanceQuery = graphql(`
 `);
 
 export function BillingOverview() {
+  const [customerId, setCustomerId] = useState("");
+
+  return (
+    <>
+      <InputCustomerId params={{ customerId, setCustomerId }} />
+      <CustomerCreditBalance params={{ customerId }} />
+    </>
+  );
+}
+
+function InputCustomerId({
+  params,
+}: {
+  params: { customerId: string; setCustomerId: (customerId: string) => void };
+}) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Customer Id input</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Input
+          type="text"
+          placeholder="Customer ID"
+          value={params.customerId}
+          onChange={(e) => params.setCustomerId(e.target.value)}
+        />
+      </CardContent>
+    </Card>
+  );
+}
+
+function CustomerCreditBalance({ params }: { params: { customerId: string } }) {
   const [result] = useQuery({
     query: CreditBalanceQuery,
-    variables: { customerId: "1234" },
+    variables: { customerId: params.customerId },
   });
-  if (result.fetching) return <p>Loading...</p>;
+  if (result.fetching) {
+    return (
+      <Card>
+        <CardHeader>
+          <CardTitle>Loading</CardTitle>
+        </CardHeader>
+      </Card>
+    );
+  }
+  if (!result.data?.billing) {
+    return (
+      <Card>
+        <CardHeader>
+          <CardTitle>No data</CardTitle>
+        </CardHeader>
+      </Card>
+    );
+  }
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Customer Credit Balance</CardTitle>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-4">
+        <div className="flex flex-row justify-between">
+          <Label>per month</Label>
+          <Label>{result.data.billing.monthlyCredits}</Label>
+        </div>
+        <div className="flex flex-row justify-between">
+          <Label>left this month</Label>
+          <Label>{result.data.billing.remainingCredits}</Label>
+        </div>
+      </CardContent>
+      <CardFooter>
+        <EditMonthlyCredits
+          params={{
+            customerId: params.customerId,
+            currentMonthlyCredit: result.data.billing.monthlyCredits,
+          }}
+        />
+      </CardFooter>
+    </Card>
+  );
+}
 
-  return <section>{JSON.stringify(result.data, null, 2)}</section>;
+function EditMonthlyCredits({
+  params,
+}: {
+  params: { customerId: string; currentMonthlyCredit: number };
+}) {
+  const [open, setOpen] = useState(false);
+  const [monthlyCredits, setMonthlyCredits] = useState(
+    params.currentMonthlyCredit,
+  );
+
+  useEffect(() => {
+    setMonthlyCredits(params.currentMonthlyCredit);
+  }, [params.currentMonthlyCredit, setMonthlyCredits]);
+
+  function onSubmit() {
+    console.log("submit", monthlyCredits);
+    // todo: make actual request
+    setOpen(false);
+  }
+
+  // todo: use form to introduce validation
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger>
+        <Button variant="outline">Edit monthly credits</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edit monthly credits</DialogTitle>
+        </DialogHeader>
+        <Input
+          value={monthlyCredits}
+          type="number"
+          onChange={(e) => setMonthlyCredits(Number(e.target.value))}
+        />
+        <DialogFooter>
+          <Button onClick={onSubmit}>Save</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@ import { BillingOverview } from "./_components/billing-overview";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <h1>Hello World</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center ">
       <BillingOverview />
     </main>
   );
